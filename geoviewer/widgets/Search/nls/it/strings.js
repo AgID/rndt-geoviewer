5d9100364c8701eeb3b5d43529b2350ceb7204c0
Antonio Rotundo <anto.rotundo@gmail.com>
2020-08-31 18:21:54 +0200

    Add files via upload

---
+++ geoviewer/widgets/Search/nls/it/strings.js
@@ -0,0 +1,11 @@
+﻿define(
+   ({
+    _widgetLabel: "Ricerca",
+    searchResult: "Risultato ricerca",
+    showAllResults: "Mostra risultati ricerca per ",
+    showAll: "Mostra risultati ricerca",
+    more: "altro",
+    untitled: "Senza titolo",
+	goecoderEsriPlaceholder: "Inserisci indirizzo"
+  })
+);
